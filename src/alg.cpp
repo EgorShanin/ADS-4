75774c702c4f456ddf25715c553be47e87f98e9c
EgorShanin <94217235+EgorShanin@users.noreply.github.com>
2022-03-25 12:18:41 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,9 +1,9 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
     int max = 0;
-    for(int i = 0; i < len; i++) {
-        for(int j = 0; j < len; j++) {
-            if(arr[i] < arr[j]) {
+    for (int i = 0; i < len; i++) {
+        for (int j = 0; j < len; j++) {
+            if (arr[i] < arr[j]) {
                 max = arr[i];
                 arr[i] = arr[j];
                 arr[j] = max;
@@ -11,9 +11,9 @@ int countPairs1(int *arr, int len, int value) {
         }
     }
     int count = 0;
-    for(int i = 0; i < len; i++) {
-        for(int j = i + 1; j < len; j++) {
-            if(arr[i] + arr[j] == value) {
+    for (int i = 0; i < len; i++) {
+        for (int j = i + 1; j < len; j++) {
+            if (arr[i] + arr[j] == value) {
                 count++;
             }
         }
@@ -22,9 +22,9 @@ int countPairs1(int *arr, int len, int value) {
 }
 int countPairs2(int *arr, int len, int value) {
     int max = 0;
-    for(int i = 0; i < len; i++) {
-        for(int j = 0; j < len; j++) {
-            if(arr[i] < arr[j]) {
+    for (int i = 0; i < len; i++) {
+        for (int j = 0; j < len; j++) {
+            if (arr[i] < arr[j]) {
                 max = arr[i];
                 arr[i] = arr[j];
                 arr[j] = max;
@@ -32,17 +32,17 @@ int countPairs2(int *arr, int len, int value) {
         }
     }
     int count = 0;
-    for(int i = len - 1; i >= 0; i--) {
-        if(arr[i] > value) {
+    for (int i = len - 1; i >= 0; i--) {
+        if (arr[i] > value) {
             len--;
         }
     }
-    for(int i = 0; i < len; i++) {
-        if(arr[i] > value / 2) {
+    for (int i = 0; i < len; i++) {
+        if (arr[i] > value / 2) {
             continue;
         }
-        for(int j = i + 1; j < len; j++) {
-            if(arr[i] + arr[j] == value) {
+        for (int j = i + 1; j < len; j++) {
+            if (arr[i] + arr[j] == value) {
                 count++;
             }
         }
@@ -51,9 +51,9 @@ int countPairs2(int *arr, int len, int value) {
 }
 int countPairs3(int *arr, int len, int value) {
     int max = 0;
-    for(int i = 0; i < len; i++) {
-        for(int j = 0; j < len; j++) {
-            if(arr[i] < arr[j]) {
+    for (int i = 0; i < len; i++) {
+        for (int j = 0; j < len; j++) {
+            if (arr[i] < arr[j]) {
                 max = arr[i];
                 arr[i] = arr[j];
                 arr[j] = max;
@@ -64,26 +64,26 @@ int countPairs3(int *arr, int len, int value) {
     int minArr = 0;
     int maxArr = 0;
     int midArr = 0;
-    for(int i = len - 1; i >= 0; i--) {
-        if(arr[i] > value) {
+    for (int i = len - 1; i >= 0; i--) {
+        if (arr[i] > value) {
             len--;
         }
     }
-    for(int i = 0; i<len; i++){
-        if(arr[i] > value / 2) {
+    for (int i = 0; i<len; i++){
+        if (arr[i] > value / 2) {
             continue;
         }
         minArr= i + 1;
         maxArr = len - 1;
-        while(minArr < maxArr) {
+        while (minArr < maxArr) {
             midArr = (minArr + maxArr) / 2;
-            if(arr[midArr] + arr[i] < value) {
+            if (arr[midArr] + arr[i] < value) {
                 minArr = midArr + 1;            
             } else {  
                 maxArr = midArr;
             }
         }
-        while(arr[minArr] + arr[i] == value) {
+        while (arr[minArr] + arr[i] == value) {
             count++;
             minArr++;
         }
